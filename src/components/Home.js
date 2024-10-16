0dfa33b04b202a78c56c9a946c28368f7b81ab74
Pratik Deshmane <pratikdeshmaneps@gmail.com>
2024-10-16 15:59:06 +0530

    Revert "Merge branch 'gh-pages' of https://github.com/pratiksdeshmane/pratikdeshmane into gh-pages"
    This reverts commit a577f329149ad2e69320c19dbc69a3ea7554c11c, reversing
    changes made to 172ca4cdde86ef8fc4e100b153dfc3ada2c85c55.

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+// src/components/MyComponent.js
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Hello from MyComponent!</h1>
+    </div>
+  );
+};
+
+export default Home;
