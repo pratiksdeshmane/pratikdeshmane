172ca4cdde86ef8fc4e100b153dfc3ada2c85c55
Pratik Deshmane <pratikdeshmaneps@gmail.com>
2024-10-16 15:57:34 +0530

    removed button component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import { Button } from '@mui/material';
 import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <Button variant="contained" color="primary">
-        My MUI Button
-      </Button>
       <Home/>
     </div>
   );
